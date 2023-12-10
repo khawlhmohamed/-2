be6ff006b03bee3c8737fc35b7d120cc7d073bfc
khawlhmohamed <152140203+khawlhmohamed@users.noreply.github.com>
2023-12-10 21:29:55 +0300

    Add files via upload

---
+++ تكليف 2/Pacage/A.java
@@ -0,0 +1,7 @@
+package Pacage;
+
+/*public interface A extends B {
+}
+// Can two interfaces mutually extend each other? Why or why not?
+
+//لايسمح بالوراثة العكسية الانها تكون وراثة دائرية ولا معنى لها بالوراثة*/
